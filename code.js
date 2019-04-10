ab98927c8a520e231690e5d77b7590abdf3ad927
IamLAM <ingeniero@luismonge.com.mx>
2019-04-10 00:15:45 -0700

    Simple function for temperature

---
+++ code.js
@@ -0,0 +1,9 @@
+//In this course you will learn  to develop simple codes with JS
+
+function convertToFarenheit(celsius) {
+    let fahrenheit;
+    fahrenheit = (celsius * 9 / 5) + 32;
+    return fahrenheit;
+}
+
+console.log(convertToFarenheit(5));
